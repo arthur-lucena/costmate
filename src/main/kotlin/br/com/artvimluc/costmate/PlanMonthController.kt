f501b84ec4d79ded156412e8a7db5d0c72eb97f5
Arthur Lucena <arthurvmx5@gmail.com>
2023-05-31 22:49:34 -0300

    fixes on controller

--- src/main/kotlin/br/com/artvimluc/costmate/PlanMonthController.kt
+++ src/main/kotlin/br/com/artvimluc/costmate/PlanMonthController.kt
@@ -1,18 +1,28 @@
 package br.com.artvimluc.costmate;
 
+import br.com.artvimluc.costmate.expense.ExpenseRepository
+import br.com.artvimluc.costmate.income.IncomeRepository
+import br.com.artvimluc.costmate.planmonth.PlanMonthDTO
+import br.com.artvimluc.costmate.planmonth.PlanMonthRepository
+import br.com.artvimluc.costmate.planmonth.PlanMonthService
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("/plan-month")
 class PlanMonthController
 
-@PostMapping
-fun  create(): ResponseEntity<String> {
-    return ResponseEntity.ok("foi")
+@Autowired
+constructor (
+    private val planMonthService: PlanMonthService,
+) {
+    @PostMapping
+    fun create(): ResponseEntity<PlanMonthDTO> {
+        return ResponseEntity.ok(planMonthService.create())
+    }
 }
-
