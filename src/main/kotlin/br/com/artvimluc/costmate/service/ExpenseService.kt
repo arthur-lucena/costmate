0c3f9b4fbddeaf4872298488613426d8391620c6
Arthur <arthur@pop-os.localdomain>
2023-10-11 08:46:17 -0300

    adding expense crud endpoints

--- src/main/kotlin/br/com/artvimluc/costmate/service/ExpenseService.kt
+++ src/main/kotlin/br/com/artvimluc/costmate/service/ExpenseService.kt
@@ -1,12 +1,13 @@
 package br.com.artvimluc.costmate.service
 
-import br.com.artvimluc.costmate.domain.CreditCard
 import br.com.artvimluc.costmate.domain.Expense
-import br.com.artvimluc.costmate.repository.CreditCardRepository
+import br.com.artvimluc.costmate.exception.NotFoundException
 import br.com.artvimluc.costmate.repository.ExpenseRepository
+import br.com.artvimluc.costmate.util.MessageLocator
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.math.BigDecimal
+
+private const val NOT_FOUND = "expense.not.found"
 
 @Service
 class ExpenseService
@@ -14,38 +15,65 @@ class ExpenseService
     @Autowired
     constructor (
         private val expenseRepository: ExpenseRepository,
-        private val creditCardRepository: CreditCardRepository,
-        private val planMonthService: PlanMonthService,
+        private val messageLocator: MessageLocator,
     ) {
 
     fun create(expense: Expense): Expense {
-        var cardAme = CreditCard(
-            name = "Cartão Ame",
-            description = "Meu cartão secundário",
-            dayDueDate = 10,
-            daysToClose = 13
-        )
+        return expenseRepository.save(expense)
+    }
 
-        cardAme = creditCardRepository.save(cardAme)
+    fun update(id: Long, expense: Expense): Expense {
+        val expenseOnBase = find(id)
 
-        val planMonth = planMonthService.findCurrent();
-
-        val expense = Expense(
-            planMonthId = planMonth.id,
-            creditCardId = cardAme.id,
-            description = "cartão nu",
-            value = BigDecimal.valueOf(3580.89),
-            installment = 1,
-            totalInstallments = 1,
-            refund = false,
-            receiptFileUploaded = false)
+        expense.id = expenseOnBase.id
 
         return expenseRepository.save(expense)
     }
 
-    fun find(year: Int?, month: Int?) : List<Expense?>? {
-        val planMonth = planMonthService.find(year, month);
+    fun delete(id: Long) {
+        val expense = find(id)
 
-        return expenseRepository.findByPlanMonthId(planMonth.id)
+        expenseRepository.delete(expense)
     }
+
+    fun find(id: Long) : Expense {
+        val expense = expenseRepository.findById(id)
+
+        return expense.orElseThrow { throw NotFoundException(
+            String.format(
+                messageLocator.getMessage(NOT_FOUND),
+                id
+            )) }
+    }
+
+//    fun create(expense: Expense): Expense {
+//        var cardAme = CreditCard(
+//            name = "Cartão Ame",
+//            description = "Meu cartão secundário",
+//            dayDueDate = 10,
+//            daysToClose = 13
+//        )
+//
+//        cardAme = creditCardRepository.save(cardAme)
+//
+//        val planMonth = planMonthService.findCurrent();
+//
+//        val expense = Expense(
+//            planMonthId = planMonth.id,
+//            creditCardId = cardAme.id,
+//            description = "cartão nu",
+//            value = BigDecimal.valueOf(3580.89),
+//            installment = 1,
+//            totalInstallments = 1,
+//            refund = false,
+//            receiptFileUploaded = false)
+//
+//        return expenseRepository.save(expense)
+//    }
+//
+//    fun find(year: Int?, month: Int?) : List<Expense?>? {
+//        val planMonth = planMonthService.find(year, month);
+//
+//        return expenseRepository.findByPlanMonthId(planMonth.id)
+//    }
 }
--- src/main/kotlin/br/com/artvimluc/costmate/web/controller/ExpenseController.kt
+++ src/main/kotlin/br/com/artvimluc/costmate/web/controller/ExpenseController.kt
@@ -3,6 +3,7 @@ package br.com.artvimluc.costmate.web.controller;
 import br.com.artvimluc.costmate.domain.Expense
 import br.com.artvimluc.costmate.service.ExpenseService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -14,14 +15,23 @@ class ExpenseController
 constructor (
     private val expenseService: ExpenseService,
 ) {
-    @PostMapping("/{referenceMonth}/{referenceYear}")
-    fun create(@PathVariable referenceYear: Int, @PathVariable referenceMonth: Int,
-               @RequestBody expense: Expense): ResponseEntity<Expense> {
-        return ResponseEntity.ok(expenseService.create(expense))
+    @GetMapping("/{id}")
+    fun get(@PathVariable id: Long): ResponseEntity<Expense> {
+        return ResponseEntity.ok(expenseService.find(id))
+    }
+    @PostMapping
+    fun create(@RequestBody expense: Expense): ResponseEntity<Expense> {
+        return ResponseEntity(expenseService.create(expense), HttpStatus.CREATED)
     }
 
-    @GetMapping("/{referenceMonth}/{referenceYear}")
-    fun get(@PathVariable referenceYear: Int, @PathVariable referenceMonth: Int): ResponseEntity<List<Expense?>?> {
-        return ResponseEntity.ok(expenseService.find(referenceYear, referenceMonth))
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Long, @RequestBody expense: Expense): ResponseEntity<Expense> {
+        return ResponseEntity(expenseService.update(id, expense), HttpStatus.OK)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long): ResponseEntity<Expense> {
+        expenseService.delete(id)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 }
